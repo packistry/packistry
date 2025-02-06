046a4fe36f6c069e64967963693a294d5c7e530e
Jamie Schouten <j4mie@hey.com>
2025-02-06 19:14:56 +0100

    Use GITHUB provider

--- tests/Feature/Incoming/GitHub/SignatureTest.php
+++ tests/Feature/Incoming/GitHub/SignatureTest.php
@@ -5,14 +5,13 @@ declare(strict_types=1);
 use App\Enums\SourceProvider;
 use App\Models\Repository;
 use App\Models\Source;
-
 use function Pest\Laravel\postJson;
 
 $event = ['ref' => 'refs/tags/0.1.3'];
 
 it('requires valid signature', function (Repository $repository) use ($event): void {
     $source = Source::factory()
-        ->provider(SourceProvider::GITLAB)
+        ->provider(SourceProvider::GITHUB)
         ->create();
 
     /** @var string $content */
@@ -24,7 +23,7 @@ it('requires valid signature', function (Repository $repository) use ($event): v
     postJson($repository->url("/incoming/github/$source->id"), $event, ['X-Hub-Signature-256' => 'incorrect'])
         ->assertUnauthorized();
 
-    $signature = 'sha256='.hash_hmac('sha256', $content, decrypt($source->secret));
+    $signature = 'sha256='.hash_hmac('sha256', $content, (string) decrypt($source->secret));
 
     postJson($repository->url("/incoming/github/$source->id"), $event, ['X-Hub-Signature-256' => $signature])
         ->assertUnprocessable();
