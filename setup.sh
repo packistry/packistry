9b1b65778dc74fa228f8bf6aa93019b3318b7923
Jamie Schouten <j4mie@hey.com>
2025-02-09 20:15:07 +0100

    Allow configuring worker numprocs with WORKER_NUMPROCS env variable

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
+envsubst < /etc/supervisord.tpl > ~/supervisord.conf
+
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
 php artisan migrate --force
 
-/usr/bin/supervisord -c /etc/supervisord.conf
+/usr/bin/supervisord -c ~/supervisord.conf
