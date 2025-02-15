a1065b8141edb50832681100042ab9f8459bf32b
Jamie Schouten <j4mie@hey.com>
2025-02-15 21:34:13 +0100

    Create separate jobs package import

---
+++ app/Jobs/ImportTags.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Jobs;
+
+use App\Models\Package;
+use App\Models\Source;
+use App\Sources\Importable;
+use App\Sources\Project;
+use Illuminate\Bus\Batchable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class ImportTags implements ShouldQueue
+{
+    use Batchable;
+    use Queueable;
+
+    public function __construct(
+        private readonly Source $source,
+        private readonly Package $package,
+        private readonly Project $project
+    ) {
+        //
+    }
+
+    public function handle(): void
+    {
+        $tags = $this->source->client()->tags($this->project);
+
+        $jobs = array_map(fn (Importable $tag) => new ImportImportable(
+            $this->source,
+            $this->package,
+            $tag,
+        ), array_reverse($tags));
+
+        $this->batch()?->add($jobs);
+    }
+}
