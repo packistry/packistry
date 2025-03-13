6f870760f7f47adbc2b5a1037626b16651eefeea
Jamie Schouten <j4mie@hey.com>
2025-03-13 21:57:46 +0100

    Fix incorrect package updated when package from same source is in different repositories (#72)

--- tests/Feature/Incoming/DeleteBranchTest.php
+++ tests/Feature/Incoming/DeleteBranchTest.php
@@ -31,3 +31,40 @@ it('deletes branch', function (Repository $repository, SourceProvider $provider,
         refType: 'heads',
         ref: 'feature-something'
     ));
+
+it('deletes branch from correct repository', function (Repository $repository, SourceProvider $provider, ...$args): void {
+    $otherRepo = Repository::factory()->create();
+
+    /** @var Package $otherPackage */
+    $otherPackage = Package::factory()
+        ->for($otherRepo)
+        ->name('vendor/test')
+        ->has(Version::factory()->name('dev-feature-something'))
+        ->provider($provider)
+        ->create();
+
+    /** @var Package $package */
+    $package = Package::factory()
+        ->for($repository)
+        ->name('vendor/test')
+        ->has(Version::factory()->name('dev-feature-something'))
+        ->state([
+            'provider_id' => $otherPackage->provider_id,
+            'source_id' => $otherPackage->source_id,
+        ])
+        ->create();
+
+    /** @var Version $version */
+    $version = Version::query()->latest('id')->first();
+
+    webhook($repository, $package->source, ...$args)
+        ->assertOk()
+        ->assertExactJson(resourceAsJson(new VersionResource($version)));
+
+    expect(Version::query()->count())->toBe(1);
+})
+    ->with(rootAndSubRepository())
+    ->with(providerDeleteEvents(
+        refType: 'heads',
+        ref: 'feature-something'
+    ));
